16848c071b2ecc63e8aedcdbe06dae07bb732060
Eric Christopherson <echristopherson@gmail.com>
2016-11-03 09:09:43 -0500

    update packages (NOTE: Emacs 25.1.1 used)

---
+++ elpa/dash-20161102.334/dash-autoloads.el
@@ -0,0 +1,17 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/dash-20161102.334/dash-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/dash-20161102.334/dash-pkg.el" "../../../.emacs.d/elpa/dash-20161102.334/dash.el")
+;;;;;;  (22555 17559 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
--- init.el
+++ init.el
@@ -143,3 +143,18 @@
 
 ;; grep-find custom executable(s) [EAC]
 (load "grep-find.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(package-selected-packages
+   (quote
+    (tagedit smex rainbow-delimiters projectile paredit magit inf-clojure ido-ubiquitous git-rebase-mode git-commit-mode exec-path-from-shell clojure-mode-extra-font-locking cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
