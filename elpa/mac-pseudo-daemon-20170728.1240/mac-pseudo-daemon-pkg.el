ed7d288dd946dea3668459e680f3d1ea2b2d7578
Eric Christopherson <echristopherson@gmail.com>
2017-11-04 23:14:50 -0500

    install mac-pseudo-daemon

---
+++ elpa/mac-pseudo-daemon-20170728.1240/mac-pseudo-daemon-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mac-pseudo-daemon" "20170728.1240" "Daemon mode that plays nice with Mac OS." '((cl-lib "0.1")) :commit "d235680a72677f11925b912428ad1a57b664e3e8" :url "https://github.com/DarwinAwardWinner/osx-pseudo-daemon" :keywords '("convenience" "osx" "mac"))
