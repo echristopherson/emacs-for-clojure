83816a5cbc8bd65ae2cd0b3574b162eb08a7764b
Eric Christopherson <echristopherson@gmail.com>
2017-11-04 19:30:31 -0500

    add blank line

--- init.el
+++ init.el
@@ -151,6 +151,7 @@
 
 ;; grep-find custom executable(s) [EAC]
 (load "grep-find.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
