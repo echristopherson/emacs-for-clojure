5b306c4ebdfa61b8265ef02bdd058e13cf7ae859
Eric Christopherson <echristopherson@gmail.com>
2017-11-04 19:30:05 -0500

    large update

---
+++ elpa/inf-clojure-20171029.40/inf-clojure-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inf-clojure" "20171029.40" "Run an external Clojure process in an Emacs buffer" '((emacs "24.4") (clojure-mode "5.6")) :commit "b04f05d2c735795b68a10cc7e699b676a8acc811" :url "http://github.com/clojure-emacs/inf-clojure" :keywords '("processes" "clojure"))
