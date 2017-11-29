e9e2dc6f430469224d8c9fca9d990b80d9983b55
Eric Christopherson <echristopherson@gmail.com>
2017-11-28 23:31:04 -0600

    try again: only call mac-pseudo-daemon-mode under macOS (or at least Darwin)

--- customizations/server-start.el
+++ customizations/server-start.el
@@ -5,5 +5,5 @@
   ;; Prevent Emacs.app from becoming unresponsive once we close all
   ;; the GUI frames Actually, mac-pseudo-daemon-mode should be defined
   ;; even outside of macOS; but currently on my Ubuntu it isn't.
-  (when (fboundp 'mac-pseudo-daemon-mode)
-    mac-pseudo-daemon-mode))
+  (when (eq system-type 'darwin)
+    (mac-pseudo-daemon-mode)))
